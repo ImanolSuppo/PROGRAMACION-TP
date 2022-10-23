614c69f8db8f020ee2ef0ff7eb055e994f903d60
ImanolSuppo <111056883+ImanolSuppo@users.noreply.github.com>
2022-10-23 11:15:46 -0300

    Add files via upload

---
+++ SistemaAcademicoSLN/SistemaAcademico/Dominio/Alumno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAcademico.Dominio
+{
+    internal class Alumno
+    {
+        public int Legajo { get; set; }
+        public Persona IdPersona { get; set; }
+        public Alumno(int legajo, Persona idPersona)
+        {
+            Legajo = legajo;
+            IdPersona = idPersona;
+        }
+        public Alumno()
+        {
+            Legajo = 000;
+            IdPersona = new Persona();
+        }
+    }
+}
---
+++ SistemaAcademicoSLN/SistemaAcademico/Dominio/Inscripcion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAcademico.Dominio
+{
+    internal class Inscripcion
+    {
+        public List<DetalleInscripcion> DetalleInscripcions { get; set; }
+        public int Legajo { get; set; }
+        public DateTime Fecha { get; set; }
+        public int Curso { get; set; }
+        public Inscripcion(List<DetalleInscripcion> detalleInscripcions, int legajo, DateTime fecha, int curso)
+        {
+            DetalleInscripcions = detalleInscripcions;
+            Legajo = legajo;
+            Fecha = fecha;
+            Curso = curso;
+        }
+        public Inscripcion()
+        {
+            DetalleInscripcions = new List<DetalleInscripcion>();
+            Legajo = 0;
+            Fecha = DateTime.Now;
+            Curso = 0;
+        }
+    }
+}
