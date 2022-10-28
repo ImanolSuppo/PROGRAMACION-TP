5de6d8e48a9c2c7f68bd50b14ec704ff942d30a5
ImanolSuppo <111056883+ImanolSuppo@users.noreply.github.com>
2022-10-28 11:16:25 -0300

    Add files via upload

---
+++ SistemaAcademicoSLN/SistemaAcademico/datos/IDao.cs
@@ -0,0 +1,17 @@
+﻿using SistemaAcademico.dominio;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace SistemaAcademico.datos
+{
+    internal interface IDao
+    {
+        IDao ObtenerInstancia(); 
+        int ObtenerProximoNro(string SP);
+        DataTable ObtenerCombo(string SP);
+        bool AltaLegajo(Alumno alumno);
+        bool AltaInscripcion(Inscripcion objeto);
+    }
+}
