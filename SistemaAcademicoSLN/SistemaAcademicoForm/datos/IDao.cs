f84cc58d7704cd1f7b4400dccfe45d780c4f29de
ImanolSuppo <111056883+ImanolSuppo@users.noreply.github.com>
2022-10-28 11:19:32 -0300

    Se crea formulario AltaUsuario

---
+++ SistemaAcademicoSLN/SistemaAcademicoForm/datos/IDao.cs
@@ -0,0 +1,17 @@
+﻿using SistemaAcademicoForm.dominio;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace SistemaAcademico.datos
+{
+    public interface IDao
+    {
+        int ObtenerProximoNro(string SP);
+        DataTable ObtenerCombo(string SP);
+        DataTable ConsultarLegajo(string SP, int leg);
+        int AltaLegajo(Persona persona);
+        bool AltaInscripcion(Inscripcion objeto);
+    }
+}
