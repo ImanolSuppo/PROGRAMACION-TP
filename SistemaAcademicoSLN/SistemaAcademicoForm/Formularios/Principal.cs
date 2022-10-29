d58ae355cfd00bc269eca4384570a934a37f4e43
ImanolSuppo <111056883+ImanolSuppo@users.noreply.github.com>
2022-10-29 10:21:55 -0300

    Add files via upload

---
+++ SistemaAcademicoSLN/SistemaAcademicoForm/Formularios/Principal.cs
@@ -0,0 +1,27 @@
+﻿using SistemaAcademico.datos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaAcademicoForm
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void nuevoUsuarioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AltaAlumno altaAlumno = new AltaAlumno();
+            altaAlumno.ShowDialog();
+        }
+    }
+}
