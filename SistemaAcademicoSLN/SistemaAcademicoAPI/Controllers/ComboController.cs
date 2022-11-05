387769c8a38f340d67e0a8ac3d89f63d9c3fefcf
ImanolSuppo <111056883+ImanolSuppo@users.noreply.github.com>
2022-11-05 09:46:43 -0300

    Add files via upload

---
+++ SistemaAcademicoSLN/SistemaAcademicoAPI/Controllers/ComboController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SistemaAcademico.dominio;
+using SistemaAcademico.fachada;
+using System.Collections.Generic;
+using System.Data;
+
+namespace SistemaAcademicoAPI.Controllers
+{
+    public class ComboController : Controller
+    {
+        private IDataApi dataApi;
+        public ComboController()
+        {
+            dataApi = new DataApi();
+        }
+        // GET: CarrerasController
+        [HttpGet("/Carreras")]
+        public ActionResult GetComboCarrera()
+        {
+            DataTable dataTable = null;
+            try
+            {
+                dataTable = dataApi.GetCombo("SP_consultar_carreras");
+                List<Carrera> lst = dataApi.ObtenerListaCarrera(dataTable);
+                return Ok(lst);
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Error interno! Intente luego");
+            }
+        }
+
+        [HttpGet("/Materias")]
+        public ActionResult GetComboMateria()
+        {
+            DataTable dataTable = null;
+            try
+            {
+                dataTable = dataApi.GetCombo("sp_consultar_materias");
+                List<Materia> lst = dataApi.ObtenerListaMateria(dataTable);
+                return Ok(lst);
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Error interno! Intente luego");
+            }
+        }
+        [HttpGet("/TipoDocumentos")]
+        public ActionResult GetComboTipoDocumento()
+        {
+            DataTable dataTable = null;
+            try
+            {
+                dataTable = dataApi.GetCombo("SP_CONSULTAR_TIPO_DOC");
+                List<TipoDocumento> lst = dataApi.ObtenerListaTipoDoc(dataTable);
+                return Ok(lst);
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Error interno! Intente luego");
+            }
+        }
+        [HttpGet("/Barrios")]
+        public ActionResult GetComboBarrio()
+        {
+            DataTable dataTable = null;
+            try
+            {
+                dataTable = dataApi.GetCombo("SP_CONSULTAR_BARRIOS");
+                List<Barrios> lst = dataApi.ObtenerListaBarrio(dataTable);
+                return Ok(lst);
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Error interno! Intente luego");
+            }
+        }
+
+        [HttpPost("/Inscripcion")]
+        public IActionResult CreateInscripcoin(Inscripcion inscripcion)
+        {
+            try
+            {
+                if (inscripcion == null)
+                {
+                    return BadRequest("Datos de presupuesto incorrectos!");
+                }
+
+                return Ok(dataApi.GuardarInscripcion(inscripcion));
+            }
+            catch
+            {
+                return StatusCode(500, "Error interno! Intente luego");
+            }
+        }
+    }
+}
---
+++ SistemaAcademicoSLN/SistemaAcademicoAPI/Controllers/TransaccionController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SistemaAcademico.dominio;
+using SistemaAcademico.fachada;
+
+namespace SistemaAcademicoAPI.Controllers
+{
+    public class TransaccionController : Controller
+    {
+        private IDataApi dataApi;
+        public TransaccionController()
+        {
+            dataApi = new DataApi();
+        }
+        // GET: TransaccionController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: TransaccionController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TransaccionController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TransaccionController/Create
+        /*[HttpPost("/Inscripcion")]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateInscripcoin(Inscripcion inscripcion)
+        {
+            try
+            {
+                if (inscripcion == null)
+                {
+                    return BadRequest("Datos de presupuesto incorrectos!");
+                }
+
+                return Ok(dataApi.GuardarInscripcion(inscripcion));
+            }
+            catch
+            {
+                return StatusCode(500, "Error interno! Intente luego");
+            }
+        }*/
+
+        // GET: TransaccionController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TransaccionController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TransaccionController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TransaccionController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
